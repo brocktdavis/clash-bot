3df242cda714e0ff2835d2b14c9653d580d18a37
Brock T Davis <btdavis9999@gmail.com>
2023-01-03 21:19:15 -0600

    Setup dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// dotenv config
+import {config} from 'dotenv';
+config();
+
 import {Client} from 'discord.js';
 
 console.log('Hello ', process.env.USER);
+console.log('token: ', process.env.BOT_TOKEN);
