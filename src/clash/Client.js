dab5396d752bb2d32d4f84afea44b401d374c9dc
Brock T Davis <btdavis9999@gmail.com>
2023-06-19 15:15:16 -0500

    Change readme and client

--- src/clash/Client.js
+++ src/clash/Client.js
@@ -2,9 +2,14 @@ import { Client } from'clashofclans.js';
 
 const ClashClient = new Client({keys: [process.env.COC_DEV_TOKEN]});
 
-ClashClient.getPlayer('<FILL>')
+ClashClient.getPlayer(process.env.COC_PLAYER_TAG)
   .then((player) => {
     console.log(`Player: ${player.name}. Clan: ${player.clan}`);
+  });
+
+ClashClient.getClan(process.env.COC_CLAN_TAG)
+  .then((clan) => {
+    console.log(`Clan: ${clan.name}`);
   })
 
 export {ClashClient};
